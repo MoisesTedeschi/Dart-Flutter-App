0a577761cdb257c7ea97aca667342be4eb315d2f
moa.tedeschi <moisestedeschi@gmail.com>
2018-12-10 17:40:11 -0200

    Otimização de código.

--- conversor_moeda_app/lib/main.dart
+++ conversor_moeda_app/lib/main.dart
@@ -6,18 +6,18 @@ import 'dart:convert';
 //Fazendo a requisição ao HG Finance
 const request = "https://api.hgbrasil.com/finance?format=json&key=b6380d87";
 
-void main() async{
+void main() async {
   //Retorna o futuro do MAP
   //print(await getData());
 
   runApp(MaterialApp(
     home: Home(),
+    theme: ThemeData(hintColor: Colors.amber, primaryColor: Colors.white),
   ));
 }
 
 //Retorna um dado futuro para a requisição - Um MAP no futuro.
-Future<Map> getData() async{
-
+Future<Map> getData() async {
   //Adiciona a resposta do servidor na variável "response", mas espera a resposta.
   //await - Espera os dados chegarem e manda para a variável.
   http.Response response = await http.get(request);
@@ -32,43 +32,105 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  final realController = TextEditingController();
+  final dolarController = TextEditingController();
+  final euroController = TextEditingController();
+
+  double dolar;
+  double euro;
+
+  void _realChanged(String text){
+
+  }
+  void _dolarChanged(String text){
+
+  }
+  void _euroChanged(String text){
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        title: Text("\$ Conversor \$"),
-        backgroundColor: Colors.amber,
-        centerTitle: true,
-      ),
-      //Vai construir a tela dependendo do que tiver no "getData()".
-      body: FutureBuilder<Map>(
-          future: getData(),
-          builder: (context, snapshot) {
-            switch (snapshot.connectionState){
-              case ConnectionState.none:
-              case ConnectionState.waiting:
-                return Center(
-                  child: Text("Carregando Dados",
-                    style: TextStyle(
-                      color: Colors.amber,
-                      fontSize: 25.0),
-                    textAlign: TextAlign.center,)
-                );
-              default:
-                if(snapshot.hasError){
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Text("\$ Conversor \$"),
+          backgroundColor: Colors.amber,
+          centerTitle: true,
+        ),
+        //Vai construir a tela dependendo do que tiver no "getData()".
+        body: FutureBuilder<Map>(
+            future: getData(),
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                case ConnectionState.waiting:
                   return Center(
-                      child: Text("Erro ao Carregar Dados :(",
-                        style: TextStyle(
-                            color: Colors.amber,
-                            fontSize: 25.0),
-                        textAlign: TextAlign.center,)
-                  );
-                }else{
-                  return Container(color: Colors.green,);
-                }
-            }
-          })
-    );
+                      child: Text(
+                    "Carregando Dados",
+                    style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                    textAlign: TextAlign.center,
+                  ));
+                default:
+                  if (snapshot.hasError) {
+                    return Center(
+                        child: Text(
+                      "Erro ao Carregar Dados :(",
+                      style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                      textAlign: TextAlign.center,
+                    ));
+                  } else {
+                    //Retornando valores do servidor.
+                    dolar =
+                        snapshot.data["results"]["currencies"]["USD"]["buy"];
+                    euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+
+                    return SingleChildScrollView(
+                      padding: EdgeInsets.all(10.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                          Icon(Icons.monetization_on,
+                              size: 150.0, color: Colors.amber),
+
+                          buildTextField("Reais", "R\$ ", realController, _realChanged),
+
+                          Divider(),
+
+                          buildTextField("Dólares", "U\$ ", dolarController, _dolarChanged),
+
+                          Divider(),
+
+                          buildTextField("Euros", "€ ", euroController, _euroChanged)
+
+                        ],
+                      ),
+                    );
+                  }
+              }
+            }));
   }
+}
+
+//Otimizando Campos de Texto
+Widget buildTextField(String label, String prefix, TextEditingController controlConverse, Function funcModification){
+  return TextField(
+
+    controller: controlConverse,
+    decoration: InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(color: Colors.amber),
+        border: OutlineInputBorder(),
+        prefixText: prefix
+    ),
+    style: TextStyle(
+      color: Colors.amber,
+      fontSize: 25.0,
+    ),
+    onChanged: funcModification,
+    
+    //Inserir apenas números no input
+    keyboardType: TextInputType.number,
+  );
 }
