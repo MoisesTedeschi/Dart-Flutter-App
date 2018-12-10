a16bc9e38cf6da2ddd8b396bd49d541a16d72e16
moa.tedeschi <moisestedeschi@gmail.com>
2018-12-10 15:30:00 -0200

    Configurações iniciais para requisição de API da HG Finance e correções básicas.

--- conversor_moeda_app/lib/main.dart
+++ conversor_moeda_app/lib/main.dart
@@ -1,4 +1,10 @@
-import '';
-void main(){
+import 'package:flutter/material.dart';
 
+//Fazendo a requisição ao HG Finance
+const request = "https://api.hgbrasil.com/finance?format=json&key=b6380d87";
+
+void main(){
+  runApp(MaterialApp(
+    home: Container(),
+  ));
 }
