8402cf780c114bd7d4086e4eaf47d1596247376d
Moisés Tedeschi <moisestedeschi@gmail.com>
2019-06-13 14:14:05 -0300

    Add logotipo no projeto e contig de estrutura.

--- frases_a_construcao_do_eu/lib/main.dart
+++ frases_a_construcao_do_eu/lib/main.dart
@@ -3,17 +3,36 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    title: "Frases - A Construção do Eu",
+      home: Home(),
+  ));
+}
 
-      home: Container(
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Frases - A Construção do Eu"),
+        backgroundColor: Colors.pinkAccent,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        // Definição de borda para design - teste.
+        decoration: BoxDecoration(
+          border: Border.all(width: 3.0, color: Colors.pink),
+        ),
         child: Column(
           children: <Widget>[
-            Text(
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-              textAlign: TextAlign.justify,
-            ),
+            Image.asset("images/logo-frases.png"),
+
           ],
         ),
       ),
-  ));
+    );
+  }
 }
