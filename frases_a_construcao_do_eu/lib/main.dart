fd9254dc953fe3aa028c2f71fccd241c8830f695
Moisés Tedeschi <moisestedeschi@gmail.com>
2019-06-13 16:18:42 -0300

    Finalizando: Adicionando novas frases no add.

--- frases_a_construcao_do_eu/lib/main.dart
+++ frases_a_construcao_do_eu/lib/main.dart
@@ -16,16 +16,30 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   // Definição de Variáveis do APP
   var _frases = [
-    "Frase 1",
-    "Frase 2",
-    "Frase 3",
-    "Frase 4"
+    "\"Obstáculos e dificuldades fazem parte da vida. E a vida é a arte de "
+        "superá-los.\" (Mestre DeRose)",
+    "\"Sem paz interior, sem calma interior, é difícil encontrar uma paz "
+        "duradoura.\" (Dalai Lama)",
+    "\"Não existe progresso na direção da liberdade suprema sem transformação, "
+        "e esse é o ponto fundamental na vida das pessoas, sejam elas "
+        "praticantes de yoga, ou não.\" (Iyengar)",
+    "\"A luta ansiosa pela felicidade é o que dá infelicidade a muita gente.\" (Hermógenes)"
   ];
   var _fraseGerada = "Clique para gerar uma nova frase!";
 
   void _gerarFrase(){
     // Gerando Números randomicos para randomizar as frases.
-    var numeroRanomico = Random().nextInt(4);
+    var numeroRanomico = Random().nextInt( _frases.length + 1 );
+
+    // Print de teste no console.
+    // print(numeroRanomico);
+
+    // Método Set altera o estado da tela - Se não utilizado as frase não vão mudar.
+    setState(() {
+      // A variável vai receber uma nova frase com base no valor gerado do random.
+      // O indice da frase vem do random.
+      _fraseGerada = _frases[ numeroRanomico ];
+    });
   }
 
   @override
@@ -49,9 +63,12 @@ class _HomeState extends State<Home> {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Image.asset("images/logo-frases.png"),
+            Image.asset(
+                "images/logo-frases.png",
+                width: 140,
+            ),
             Text(
-              "Clique para gerar uma nova frase!",
+              _fraseGerada,
               textAlign: TextAlign.center,
               style: TextStyle(
                 fontSize: 16.0,
@@ -61,7 +78,7 @@ class _HomeState extends State<Home> {
             ),
             RaisedButton(
               child: Text(
-                _fraseGerada,
+                "Nova Frase",
                 style: TextStyle(
                   fontSize: 16.0,
                   color: Colors.white,
@@ -69,7 +86,7 @@ class _HomeState extends State<Home> {
                 ),
               ),
               color: Colors.pink,
-              onPressed: (){},
+              onPressed: _gerarFrase,
             ),
 
           ],
