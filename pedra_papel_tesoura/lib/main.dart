7e5efce20e9c9121fcadf85fdd53648d7a573929
Moisés Tedeschi <moisestedeschi@gmail.com>
2019-06-13 17:50:50 -0300

    Import de classe jogo para o principal.

--- pedra_papel_tesoura/lib/main.dart
+++ pedra_papel_tesoura/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:pedra_papel_tesoura/Jogo.dart';
+
 
 void main() {
   runApp(MaterialApp(
     //debugShowCheckedModeBanner: false,
-    home: ,
+    home: Jogo(),
 
   ));
 }
