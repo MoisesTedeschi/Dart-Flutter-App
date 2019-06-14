4edac598e31c3f8f248698b30e7802ab8aa3b724
Moisés Tedeschi <moisestedeschi@gmail.com>
2019-06-13 23:15:45 -0300

    Criando captação de gestos nas imagens e criando métodos de escolha do usuário.

--- pedra_papel_tesoura/lib/Jogo.dart
+++ pedra_papel_tesoura/lib/Jogo.dart
@@ -6,11 +6,23 @@ class Jogo extends StatefulWidget {
 }
 
 class _JogoState extends State<Jogo> {
+
+  var _imagemAPP = AssetImage("images/padrao.png");
+
+  void opcaoSelecionada(String escolhaUsuario){
+
+
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("JoKenPo - Pedra, Papel e Tesoura"),
+        title: Text(
+            "JoKenPo - Pedra, Papel e Tesoura",
+            textAlign: TextAlign.center,
+        ),
         backgroundColor: Colors.blue,
       ),
       body: Column(
@@ -20,7 +32,7 @@ class _JogoState extends State<Jogo> {
 
         children: <Widget>[
           Padding(
-            padding: EdgeInsets.only(top: 30.0, bottom: 16.0),
+            padding: EdgeInsets.only(top: 32.0, bottom: 16.0),
             child: Text(
               "Ecolha do Oponente",
               textAlign: TextAlign.center,
@@ -31,7 +43,41 @@ class _JogoState extends State<Jogo> {
             ),
           ),
 
-          Image.asset("images/padrao.png"),
+          Image(image: this._imagemAPP, height: 100,),
+
+          Padding(
+            padding: EdgeInsets.only(top: 32, bottom: 16.0),
+            child: Text(
+              "Escolha uma opção abaixo",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              GestureDetector(
+                onTap: () => opcaoSelecionada("Pedra"),
+                child: Image.asset("images/pedra.png", height: 100,),
+              ),
+              GestureDetector(
+                onTap: () => opcaoSelecionada("Papel"),
+                child: Image.asset("images/papel.png", height: 100,),
+              ),
+              GestureDetector(
+                onTap: () => opcaoSelecionada("Tesoura"),
+                child: Image.asset("images/tesoura.png", height: 100,),
+              ),
+              /*
+              Image.asset("images/pedra.png", height: 100,),
+              Image.asset("images/papel.png", height: 100,),
+              Image.asset("images/tesoura.png", height: 100,),
+              */
+            ],
+          ),
 
         ],
       ),
